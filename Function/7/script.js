eede6d9dfc59d7c2f6eb1da61f6429c953f93500
WonderSi <kirillkozlov00@gmail.com>
2025-02-14 23:46:47 +0700

    create task 7

---
+++ Function/7/script.js
@@ -0,0 +1,21 @@
+function SequenceMember(n, sum = 0, index = 1) {
+    if (index > n) {
+        return sum;
+    }
+
+    return SequenceMember(n, sum + Math.sin(index), index + 1);
+}
+
+function SevenDisplayInfo() {
+    const num = parseFloat(document.getElementById("SevenInput").value);
+    const output = document.getElementById("SevenOutput");
+
+
+    if (isNaN(num) || num <= 0) {
+        output.innerHTML = "Введите корректное число!";
+        return;
+    }
+
+    output.innerHTML = `Вывод: ${SequenceMember(num)}`
+
+}
