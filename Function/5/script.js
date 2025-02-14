f1669f88940ecef7cfed7793a89f60de882964e0
WonderSi <kirillkozlov00@gmail.com>
2025-02-14 20:28:20 +0700

    create task 5

---
+++ Function/5/script.js
@@ -0,0 +1,10 @@
+function FiveDisplayInfo() {
+    const x = document.getElementById("FiveInput1").value;
+    const y = document.getElementById("FiveInput2").value;
+    const z = document.getElementById("FiveInput3").value;
+    const output = document.getElementById("FiveOutput");
+
+    let result = (Math.max(x,y) + Math.max(x + y,z)) / (Math.pow(Math.max(0.5,x + z), 2));
+
+    output.innerHTML = `<p>Результат формулы: ${result}</p>`
+}
