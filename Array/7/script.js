6ae308f090b2a3e8d7bae4aae48f0843155927b0
WonderSi <kirillkozlov00@gmail.com>
2025-02-19 22:37:11 +0700

    create array task 7

---
+++ Array/7/script.js
@@ -0,0 +1,7 @@
+function SevenDisplayInfo() {
+    console.log("Седьмое задани");
+    let arr = ['c', 5, 2, 'b', 3, 1, 4, 'a'];
+    console.log(`До: ${arr}`);
+    let result = arr.sort();
+    console.log(`После: ${result}`);
+}
