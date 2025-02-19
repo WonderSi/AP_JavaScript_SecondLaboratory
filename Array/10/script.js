e3be26edf3f5dc4c3ca0a2975ccdb377b1c19acc
WonderSi <kirillkozlov00@gmail.com>
2025-02-19 23:40:01 +0700

    create array task 10

--- Array/10/script.js
+++ Array/10/script.js
@@ -1,7 +1,19 @@
-function swapMinMax(arr) {
+function SwapMinMax(arr) {
+    let maxIndex = arr.indexOf(Math.max(...arr));
+    let minIndex = arr.indexOf(Math.min(...arr));
 
+    [arr[maxIndex], arr[minIndex]] = [arr[minIndex], arr[maxIndex]];
+
+    return arr;
 }
 
 function TenDisplayInfo() {
     output = document.getElementById("TenOutput");
+    let arr = [];
+    for (let i = 0; i < 10; i++) {
+        arr.push(Math.floor(Math.random() * 100));
+    }
+    output.innerHTML = '';
+    output.innerHTML += `<p>Изначальный массив: ${arr.join(", ")}</p>`;
+    output.innerHTML += `<p>Измененный массив: ${SwapMinMax(arr).join(", ")}</p>`;
 }
