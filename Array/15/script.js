809ff0e1fa77fcb266b70d4baccd04a8f09d7ed2
WonderSi <kirillkozlov00@gmail.com>
2025-02-20 01:38:52 +0700

    create array task 15

---
+++ Array/15/script.js
@@ -0,0 +1,20 @@
+let booksByAuthors = {
+    "Пушкин": ["Евгений Онегин", "Александр Сергеевич Пушкин"],
+    "Есенин": ["Собрание стихотворений", "Истоки"],
+    "Данцова": ["Книга о любви"]
+};
+
+function DisplayBooks(booksByAuthors) {
+    for (let author in booksByAuthors) {
+        console.log(`Автор: ${author}`);
+        console.log("Книги:");
+        booksByAuthors[author].forEach(book => {
+            console.log(`- ${book}`);
+        });
+        console.log("");
+    }
+}
+
+function FifteenDisplayInfo() {
+    DisplayBooks(booksByAuthors)
+}
